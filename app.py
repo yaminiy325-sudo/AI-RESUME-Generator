1bf7f522585ffff940eadf7e6f15a924c4b355df
yaminiy325-sudo <yaminiy325@gmail.com>
2026-07-27 13:14:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def prompt_generator(agent):
     with open(file_name, 'w') as f:
      f.write(response.content [-1] ['text'])
     return "Prompt file generated Successfully, agent can read it"
-
+prompt_generator(model)
 # TOOL 2:
 def resume_maker_prompt():
   """This function just gives updated prompt for model"""
@@ -79,6 +79,7 @@ def resume_maker_prompt():
   with open('prompt.py', 'r') as f:
     prompt = f.read()
   return prompt
+ resume_maker_prompt()   
 #============generate resume============
 prompt = """You are a helpful AI assistant with job resume maker, your task
 is to give HTML format resume, with proper designing using recent CSS and JS
@@ -103,7 +104,9 @@ if st.button("generate resume"):
     response = agent.invoke({'messages': [{'role': 'user',"content": query}]})
     code = response['messages'] [-1].content [-1] ['text']  
 
-    st.markdown(code)
+# st.markdown(code)
+st.html(code,width="stretch",unsafe_allow_javascript=True)
+
 
 
  
