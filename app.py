479f85eedfc8aa81b9f19f8915af9ee4259ba1f8
yaminiy325-sudo <yaminiy325@gmail.com>
2026-07-27 12:37:35 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,110 @@
+import streamlit as st 
+# streamlit: web based app making 
+# lite python framework 
+
+st.title("AI RESUME MAKER")
+
+st.markdown("""## user can create or download AI created resume based on high ats score""")
+
+
+
+
+#===================agent code======================
+# step 2: load modules
+import IPython as ip
+import os
+import time
+import langchain
+from langchain.agents import create_agent
+from langchain_groq import ChatGroq
+from langchain_google_genai import ChatGoogleGenerativeAI
+import pytesseract as pyt
+from tavily import TavilyClient
+from langchain.messages import SystemMessage, HumanMessage
+import numpy as np
+import streamlit as st
+from langchain_community.document_loaders import PyMuPDFLoader
+
+#================API KEY LOAD==============
+
+GOOGLE_API_KEY = st.sidebar.textinput("GOOGLE_API_KEY",type="password")
+GROQ_API_KEY = st.sidebar.textinput("GROQ_API_KEY",type="password")
+TAVILY_API_KEY = st.sidebar.textinput("TAVILY_API_KEY",type="password")
+#===========model building============
+model = ChatGoogleGenerativeAI(
+    model='gemini-3.5-flash-lite',
+    google_api_key = GOOGLE_API_KEY
+)
+
+# TOOL
+def search_recent_news_jobs(query):
+  """This function is helps to search recent news or recent jobs
+  related to given search query suppose user write python developer jobs
+  It should return trending news and jobs link"""
+  client = TavilyClient(api_key= TAVILY_API_KEY)
+  return client.search(query)
+
+
+  # Agent Creation
+from langchain.agents import create_agent
+agent = create_agent(
+  model=model,
+    tools= [search_recent_news_jobs] # user can give multiple tools
+)
+
+
+#=============promt generator============
+def prompt_generator(agent):
+    """ This function helps to give detailed prompt followed by chain of thoughts
+   and persona based prompting, main task is to give detailed prompt to build resume for students or Experienced
+   person Based on their given personal information"""
+
+    prompt = """You are a senior HR resume analyzer,
+   main task is to give detailed prompt to build resume for
+   students or Experienced
+   person Based on their given personal information.
+   System Instruction I want to
+   generate resume in HTML format include that in prompt"""
+
+    response = agent.invoke(prompt)
+    file_name = 'prompt.py'
+    with open(file_name, 'w') as f:
+     f.write(response.content [-1] ['text'])
+    return "Prompt file generated Successfully, agent can read it"
+
+# TOOL 2:
+def resume_maker_prompt():
+  """This function just gives updated prompt for model"""
+
+  with open('prompt.py', 'r') as f:
+    prompt = f.read()
+  return prompt
+#============generate resume============
+prompt = """You are a helpful AI assistant with job resume maker, your task
+is to give HTML format resume, with proper designing using recent CSS and JS
+code, with professional design format. user will upload data and return HTML
+format resume"""
+
+final_prompt = prompt + resume_maker_prompt()
+user_details = """User details: given below:
+YAMINI, +91 8595509031, yaminiy325@gmail.com
+rohini, Delhi DOB 13-11-2007
+Skills: Python,Web developing, canva Expert, Sql
+Languages: Hindi, english
+Education: IITM university
+Bachelor of Computer Application
+Give Python Developer Resume, always use different styling use gradient theme pallete contrast in resume"""
+
+query = final_prompt + user_details
+
+if st.button("generate resume"):
+  with st,spinner("running agent..."):
+    
+    response = agent.invoke({'messages': [{'role': 'user',"content": query}]})
+    code = response['messages'] [-1].content [-1] ['text']  
+
+    st.markdown(code)
+
+
+ 
+
